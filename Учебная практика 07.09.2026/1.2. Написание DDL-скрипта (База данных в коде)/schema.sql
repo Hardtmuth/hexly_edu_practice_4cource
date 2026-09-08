0d0d1b18c20ed426b0afb49425e6e7a4abfb60e7
Andrey Abramov <abramow-a.v@ya.ru>
2026-09-08 22:08:06 +0300

    add for check files

---
+++ Учебная практика 07.09.2026/1.2. Написание DDL-скрипта (База данных в коде)/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS partners (
+	partner_id         INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	company_name       VARCHAR(255) NOT NULL,
+	inn                VARCHAR(12) NOT NULL UNIQUE,
+	contact_email      VARCHAR(255) NOT NULL UNIQUE,
+	phone 		       VARCHAR(20) NOT NULL DEFAULT 'Not specified',
+	address            TEXT NOT NULL DEFAULT 'Not specified',
+	rating             NUMERIC(10, 2) NOT NULL CHECK (rating >= 1 AND rating <= 5) DEFAULT 3,
+	created_at         TIMESTAMP NOT NULL DEFAULT NOW(),
+  	updated_at         TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS sales (
+	sale_id      INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	partner_id   INT NOT NULL REFERENCES partners(partner_id) ON DELETE CASCADE,
+	product_name VARCHAR(255) NOT NULL,
+	sale_date    DATE NOT NULL,
+	quantity     INT NOT NULL CHECK (quantity > 0),
+	total_amount NUMERIC(10, 2) NOT NULL,
+	created_at   TIMESTAMP NOT NULL DEFAULT NOW(),
+  	updated_at   TIMESTAMP NOT NULL DEFAULT NOW()
+);
