1fad0dc14b916d0de8a0d399c8b9ceabef628d88
Andrey Abramov <abramow-a.v@ya.ru>
2026-09-10 23:42:22 +0300

    add check in add_data script, add queries.sql

--- Учебная практика 07.09.2026/1.3. Подготовка данных и импорт (ETL)/add_data.mjs
+++ Учебная практика 07.09.2026/1.3. Подготовка данных и импорт (ETL)/add_data.mjs
@@ -14,7 +14,7 @@ const reset = '\x1b[0m'
 const pgHost = process.env.PG_HOST || 'localhost'
 const pgPort = process.env.PG_PORT ||  5436
 const pgDb   = process.env.PG_DB   || 'practice'
-const pgUser = process.env.PG_USER || 'postrges'
+const pgUser = process.env.PG_USER || 'postgres'
 const pgPass = process.env.PG_PASS || 'password'
 
 const scriptPath = fileURLToPath(import.meta.url)
@@ -22,7 +22,7 @@ const dir = path.dirname(scriptPath)
 const fileNameList = fs.readdirSync(dir).filter(f => path.extname(f).toLowerCase() === '.csv')
 
 const normalizeCsv = (csvFileNameList) => {
-  console.log(`${yellow} ------- Norlamize CSV files ------ ${reset}\n`)
+  console.log(`${yellow} ------- Norlamize CSV files ------- ${reset}\n`)
   csvFileNameList.forEach(file => {
     const filePath = path.join(dir, file)
 
@@ -39,15 +39,13 @@ const normalizeCsv = (csvFileNameList) => {
 }
 
 const importData = async (normalizedFileList) => {
-  console.log(`\n${yellow} ------- Import data from CSV files ------ ${reset}\n`)
+  console.log(`\n${yellow} ------- Import data from CSV ------ ${reset}\n`)
   for(const f of normalizedFileList) {
     const table = f.split('_')[2].split('.')[0]
     const header = fs.readFileSync(f, 'utf8').split(/\r?\n/)[0].replaceAll(';', ', ')
-    // console.log(header)
     const filePath = path.resolve(dir, f)
-    // console.log(filePath)
 
-    const args = [
+    const importArgs = [
       `PGPASSWORD=${pgPass}`,
       'psql',
       `-h ${pgHost}`,
@@ -56,14 +54,45 @@ const importData = async (normalizedFileList) => {
       `-d ${pgDb}`,
       '-w',
       '-c',
-      `"\\copy ${table}(${header}) FROM '${filePath}' WITH (FORMAT csv, HEADER true, DELIMITER ';')"`
+      `"\\COPY ${table}(${header}) FROM '${filePath}' WITH (FORMAT csv, HEADER true, DELIMITER ';')"`
     ]
 
-    const cmd = args.join(' ')
+    const importCmd = importArgs.join(' ')
 
     try {
-      const { stdout } = await execPromise(cmd)
-      console.log(`${f} - ${green} success ${reset}, exec result: ${stdout}`)
+      const { stdout } = await execPromise(importCmd)
+      console.log(`${f} - ${green} success ${reset}, exec result: ${stdout.trim()}`)
+    } catch (e) {
+      console.log(`${red} [Error] ${reset}\n`)
+      console.log(e.message)
+    }
+  }
+}
+
+const checkData = async (normalizedFileList) => {
+  console.log(`\n${yellow} ------- Check data in tables ------ ${reset}\n`)
+  for(const f of normalizedFileList) {
+    const table = f.split('_')[2].split('.')[0]
+    const content = fs.readFileSync(f, 'utf8')
+    const dataRowsCount = content.split(/\r?\n/).length - 1
+
+    const checkArgs = [
+      `PGPASSWORD=${pgPass}`,
+      'psql',
+      `-h ${pgHost}`,
+      `-p ${pgPort}`,
+      `-U ${pgUser}`,
+      `-d ${pgDb}`,
+      '-w',
+      '-c',
+      `"SELECT COUNT(*) FROM ${table}"`
+    ]
+
+    const checkCmd = checkArgs.join(' ')
+
+    try {
+      const { stdout } = await execPromise(checkCmd)
+      console.log(`${f} - ${green} success ${reset} | (rows in table '${table}': ${stdout.split(/\r?\n/)[2].trim()}, expected: ${dataRowsCount})`)
     } catch (e) {
       console.log(`${red} [Error] ${reset}\n`)
       console.log(e.message)
@@ -73,3 +102,5 @@ const importData = async (normalizedFileList) => {
 
 normalizeCsv(fileNameList)
 await importData(fileNameList)
+await checkData(fileNameList)
+console.log(`\n${yellow} ----------------------------------- ${reset}\n`)
---
+++ Учебная практика 07.09.2026/1.4. Разработка проверочных SQL-запросов (Эмуляция бэкенда)/queries.sql
@@ -0,0 +1,65 @@
+/*
+1. Запрос для вывода списка партнеров с сортировкой по названию
+и выводом общего количества сделанных ими доставок
+(используйте LEFT JOIN и COUNT)
+*/
+
+SELECT
+    p.partner_id,
+    p.company_name,
+    COUNT(s.sale_id) AS total_sales,
+    p.rating,
+    p.contact_email,
+    p.phone,
+    p.created_at
+FROM partners p
+LEFT JOIN sales s ON p.partner_id = s.partner_id
+GROUP BY
+    p.partner_id
+ORDER BY
+    p.company_name ASC;
+
+/*
+2. Запрос для добавления/обновления данных
+демонстрация транзакции: создание нового партнера и запись о его первой тестовой доставке
+в рамках одного блока BEGIN...COMMIT.
+*/
+
+BEGIN;
+-- Обновление счетчиков после ручного импорта данных из CSV-файлов
+SELECT setval(
+    pg_get_serial_sequence('partners', 'partner_id'),
+    COALESCE(MAX(partner_id), 1)
+)
+FROM partners;
+
+SELECT setval(
+    pg_get_serial_sequence('sales', 'sale_id'),
+    COALESCE(MAX(sale_id), 1)
+)
+FROM sales;
+
+-- Создание нового партнера
+WITH new_partner AS (
+    INSERT INTO partners (company_name, inn, contact_email, phone)
+    VALUES ('ООО Сименс Мобайл', '7072203406', 'mobile@siemens.ru', '+79209209200')
+    RETURNING partner_id
+)
+-- Запись о его первой тестовой доставке
+INSERT INTO sales (partner_id, product_id, quantity)
+SELECT partner_id, 1, 22
+FROM new_partner;
+
+COMMIT;
+
+/*
+3. Запрос для истории реализации:
+вывод детальной истории отгрузок конкретного партнера
+за указанный период
+(с названиями продуктов, датами, объемами в штуках и итоговой суммой поставки).
+*/
+
+SELECT * FROM sales_history
+WHERE partner_inn = '7701234567'
+  AND sale_date BETWEEN '2026-01-01' AND '2026-12-31'
+ORDER BY sale_date DESC;
